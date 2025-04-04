b246ad7448c6bd0c6fdac49fd3487edef34eff8c
KChan-ka <thuntor@hotmail.com>
2025-04-04 12:54:31 +0100

    minor css

--- components/ArticleCard.jsx
+++ components/ArticleCard.jsx
@@ -9,7 +9,7 @@ export default function ArticleCard({ article }) {
             {article.article_img_url ? <img src={article.article_img_url} className="articleCardImage" /> : null}
             < h3 > {article.title}</h3 >
             <div className="articleCardDetails">
-                <p className="articleCardDetails">By, {article.author}, posted on {convertISO8601ToStandardDateTime(article.created_at)}</p>
+                <p className="articleCardDetails">By, {article.author}, {convertISO8601ToStandardDateTime(article.created_at)}</p>
                 <p className="articleCardDetails">comments: {article.comment_count}, votes: {article.votes}</p>
             </div>
         </Link >
